b323d014115ea3c026cb803ca73fcdd2075035c6
Lina Jamal <linahjamal89@gmail.com>
2020-02-26 16:25:43 +0200

    edit database schema related #9

--- src/database/config/dataBase.sql
+++ src/database/config/dataBase.sql
@@ -1,28 +1,39 @@
 BEGIN;
 
-DROP TABLE IF EXISTS menu, users, order cascade;
+DROP TABLE IF EXISTS orders,users, user_order cascade;
 
 CREATE TABLE orders (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(100) NOT NULL,
-  image VARCHAR(250),
-  description TEXT NOT NULL,
+  orderName VARCHAR(100) NOT NULL,
+  orderImage VARCHAR(250),
+  orderDescription TEXT NOT NULL,
   price INTEGER
 );
 
+INSERT INTO orders (orderName, orderImage,orderDescription,price) VALUES ('coffee', '', 'choose your favorite coffee and enjoy it fresh',8);
+INSERT INTO orders (orderName, orderImage,orderDescription,price) VALUES ('tea', '','choose your favorite tea and enjoy it fresh',5);
+
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(100) NOT NULL,
+  userName VARCHAR(100) NOT NULL
 );
+
+ INSERT into users(userName) VALUES ('soha');
+ INSERT into users(userName) VALUES ('ahmed');
+ INSERT into users(userName) VALUES ('salma');
+
+
 CREATE TABLE user_order (
   id SERIAL PRIMARY KEY NOT NULL,
-  users_id FOREIGN KEY(users.id),
-  orders_id FOREIGN KEY(orders.id),
+  users_id  int references users(id),
+  orders_id  int references orders(id),
   quantity INTEGER,
   size VARCHAR(100),
   suger VARCHAR(100),
   time_of_order TIMESTAMP , 
-  total_price INTEGER,
+  total_price INTEGER
 );
+INSERT INTO user_order (users_id, orders_id,quantity,size,suger,time_of_order,total_price) VALUES (1, 1,3,'small','one',null,24);
+INSERT INTO user_order (users_id, orders_id,quantity,size,suger,time_of_order,total_price) VALUES (1, 2,2,'small','one',null,10);
 
 COMMIT;
