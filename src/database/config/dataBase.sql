8f7ad2519549ba0b2bd0f4609c62989db46985a4
Lina Jamal <linahjamal89@gmail.com>
2020-02-27 10:30:33 +0200

    edit schema data base # 9

--- src/database/config/dataBase.sql
+++ src/database/config/dataBase.sql
@@ -1,39 +1,29 @@
 BEGIN;
 
-DROP TABLE IF EXISTS orders,users, user_order cascade;
+DROP TABLE IF EXISTS orders,menu cascade;
 
-CREATE TABLE orders (
+CREATE TABLE menu (
   id SERIAL PRIMARY KEY NOT NULL,
-  orderName VARCHAR(100) NOT NULL,
-  orderImage VARCHAR(250),
-  orderDescription TEXT NOT NULL,
+  menuName VARCHAR(100) NOT NULL,
+  menuImage VARCHAR(250),
+  menuDescription TEXT NOT NULL,
   price INTEGER
 );
 
-INSERT INTO orders (orderName, orderImage,orderDescription,price) VALUES ('coffee', '', 'choose your favorite coffee and enjoy it fresh',8);
-INSERT INTO orders (orderName, orderImage,orderDescription,price) VALUES ('tea', '','choose your favorite tea and enjoy it fresh',5);
+INSERT INTO menu (menuName, menuImage,menuDescription,price) VALUES ('coffee', '', 'choose your favorite coffee and enjoy it fresh',8);
+INSERT INTO menu (menuName, menuImage,menuDescription,price) VALUES ('tea', '','choose your favorite tea and enjoy it fresh',5);
 
-CREATE TABLE users (
+
+CREATE TABLE orders (
   id SERIAL PRIMARY KEY NOT NULL,
-  userName VARCHAR(100) NOT NULL
-);
-
- INSERT into users(userName) VALUES ('soha');
- INSERT into users(userName) VALUES ('ahmed');
- INSERT into users(userName) VALUES ('salma');
-
-
-CREATE TABLE user_order (
-  id SERIAL PRIMARY KEY NOT NULL,
-  users_id  int references users(id),
-  orders_id  int references orders(id),
+  menuId INTEGER references menu(id) NOT NULL,
   quantity INTEGER,
   size VARCHAR(100),
   suger VARCHAR(100),
   time_of_order TIMESTAMP , 
   total_price INTEGER
 );
-INSERT INTO user_order (users_id, orders_id,quantity,size,suger,time_of_order,total_price) VALUES (1, 1,3,'small','one',null,24);
-INSERT INTO user_order (users_id, orders_id,quantity,size,suger,time_of_order,total_price) VALUES (1, 2,2,'small','one',null,10);
+INSERT INTO orders (menuId,quantity,size,suger,time_of_order,total_price) VALUES (1,3,'small','one',null,24);
+INSERT INTO orders (menuId,quantity,size,suger,time_of_order,total_price) VALUES (2,2,'small','one',null,10);
 
 COMMIT;
