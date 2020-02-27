bd11537e839b7d4df116292d5590aed7365c590a
Lina Jamal <linahjamal89@gmail.com>
2020-02-27 11:18:47 +0200

    getData related #17

--- src/routes/index.router.js
+++ src/routes/index.router.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
 const { home } = require('../controller/index.controller');
+const { getData } = require('../database/queries/getData');
 
-router.use('/', home);
-
+// router.use('/', home);
+router.get('/data', (req, res) => {
+  res.json(getData().then((resu) => resu.rows)
+    .catch((e) => console.log(`error >>> ${e}`)));
+});
 module.exports = router;
