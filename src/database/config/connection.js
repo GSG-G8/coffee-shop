246b8256d3b1a3d8f3522d1baca894d70f5b6e00
Hassan Al-najjar <ha77an19@gmail.com>
2020-02-29 09:51:44 +0200

    create database url from heroku and deblaying it #12 & #7

--- src/database/config/connection.js
+++ src/database/config/connection.js
@@ -3,13 +3,12 @@ require('env2')('./config.env');
 
 let dbUrl = '';
 if (process.env.NODE_ENV === 'production') {
-  dburl = process.env.DATABASE_URL;
+  dbUrl = process.env.DATABASE_URL;
 } else if (process.env.NODE_ENV === 'test') {
-  // dbUrl = process.env.TEST_DB_URL;
+  dbUrl = process.env.TEST_DB_URL;
 } else if (process.env.NODE_ENV === 'development') {
   dbUrl = process.env.DEV_URL;
 }
-// if (!dbUrl) throw new Error('No DB URL found !!!');
 const options = {
   connectionString: dbUrl,
   ssl: true,
